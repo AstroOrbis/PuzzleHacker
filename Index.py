760b20aafb9f8823c2258497f0c63d896ad36af9
AstroOrbis <astroorbis@gmail.com>
2021-01-04 22:56:14 -0500

    Checking for var PuzzleType and if the user wants to Encrypt/Decrypt

--- Index.py
+++ Index.py
@@ -13,9 +13,18 @@ time.sleep (1)
 
 #Selection screen
 print("Choose the number that corresponds to the puzzle you want to solve!")
+time.sleep (.5)
 print("1. Cesarean Cypher")
 
 #input for var PuzzleType
 print("Type the number corresponding to your puzzle, then press enter!!")
 puzzleType = input(" ")
 print(puzzleType)
+time.sleep (2)
+#Now to check if they want to Encrypt or Decrypt...
+print("Would you like to encrypt or decrypt? Respond 'Encrypt' or 'Decrypt'. ")
+EncryptDecrypt = input(" ")
+print(EncryptDecrypt)
+time.sleep (2)
+
+#...and redirect them to the correct file using trash spaghet code

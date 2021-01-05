e1202d401461c51bf34675a1310a55d899cf749d
AstroOrbis <astroorbis@gmail.com>
2021-01-04 20:33:29 -0500

    Introduce var puzzleType for later use

--- Index.py
+++ Index.py
@@ -1,11 +1,21 @@
+
+#Imports
 import time
 
-
+#Start of the script
 print("PuzzleHacker, a script made by Astro Orbis")
-
+print(" ")
 time.sleep (1)
 
 print("Sub to me on YouTube (Astro Orbis)")
+print(" ")
+time.sleep (1)
 
-time.sleep (.65)
+#Selection screen
+print("Choose the number that corresponds to the puzzle you want to solve!")
+print("1. Cesarean Cypher")
 
+#input for var PuzzleType
+print("Type the number corresponding to your puzzle, then press enter!!")
+type = input(" ")
+print(type)
